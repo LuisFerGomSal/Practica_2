2b53bb45e61802486dff17028420a73c0e81e4b8
Luis Fernando Gomez Saldaña <fernando1894@hotmail.com>
2021-03-02 18:53:27 -0800

    creacion de proyecto para calcular perimetro de triangulo

---
+++ Practica_2/CalculoPerimetro/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculoPerimetro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //declaracion de variables
+            int a = 10;
+            int b = 10;
+            int c = 7;
+
+            //suma de variables
+            int perimetro = a + b + c;
+
+            //impresion de variables
+            Console.WriteLine("El perimetro del triangulo es: " + perimetro);
+            Console.WriteLine("Presiona cualquier tecla para continuar...");
+            Console.ReadKey();
+
+        }
+    }
+}
