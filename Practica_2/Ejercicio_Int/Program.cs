560eb42259b130c5b73c428c6c0c3a8c65a57978
Luis Fernando Gomez Saldaña <fernando1894@hotmail.com>
2021-03-02 18:43:20 -0800

    Creacion de proyecto suma de variables e impresion

---
+++ Practica_2/Ejercicio_Int/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_Int
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // declaración de variables int con valor estatico
+            int numero1 = 5;
+            int numero2 = 6;
+            int numero3 = 7;
+
+
+            //sumar las variables
+            int suma = numero1 + numero2 + numero3;
+
+            //imprimir en pantalla
+            Console.WriteLine("El valor de suma es: " + suma);
+            Console.WriteLine("Presiona cualquier tecla para continuar...");
+            Console.ReadKey(); //se sale cuando detecta la presion de una tecla
+        }
+    }
+}
