658c671f86bf4a221786c4585b2fa8792ef0616a
Luis Fernando Gomez Saldaña <fernando1894@hotmail.com>
2021-03-02 19:22:14 -0800

    creacion de proyecto que recibe nombre y edad, e imprime ambos

---
+++ Practica_2/Ejercicio_StringInt/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_StringInt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //delcracion de variables
+            String nombre;
+            String edad;
+
+            Console.WriteLine("ingresa tu nombre: ");
+            nombre = Console.ReadLine();
+
+            Console.WriteLine("ingresa tu edad: ");
+            edad = Console.ReadLine();
+
+            //mostrar en pantalla
+            Console.WriteLine("te llamas " + nombre + " y tu edad es " + edad);
+            Console.WriteLine("Presiona una tecla para continuar...");
+            Console.ReadLine();
+        }
+    }
+}
