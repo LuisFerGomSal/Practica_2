2b9b1b528df1fdaeae12fe5ea8c964f44f43bde8
Luis Fernando Gomez Saldaña <fernando1894@hotmail.com>
2021-03-05 01:40:08 -0800

    proyectos de operaciones y ambuguesas anadidos

---
+++ Practica_2/Burguer/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Burguer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //declaracion de variables
+            int cantBur, cantPap, cantBeb;
+            double total;
+            String linea;
+            const double precioBur = 45;
+            const double precioPap = 15;
+            const double precioBeb = 20;
+
+            //captura de datos
+            Console.WriteLine("Ingresa el numero de amburguesas: ");
+            linea = Console.ReadLine();
+            cantBur = int.Parse(linea);
+
+            Console.WriteLine("Ingresa el numero de papas: ");
+            linea = Console.ReadLine();
+            cantPap = int.Parse(linea);
+
+            Console.WriteLine("Ingresa el numero de bebidas: ");
+            linea = Console.ReadLine();
+            cantBeb = int.Parse(linea);
+
+            //calculo total
+            total = (cantBur * precioBur) + (cantPap * precioPap) + (cantBeb * precioBeb);
+            Console.WriteLine("El total es {0}.", total);
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ Practica_2/Operaciones/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operaciones
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //delcracion de variables
+            String n1;
+            String n2;
+
+            //captura de datos
+            Console.WriteLine("ingresa numero1: ");
+            n1 = Console.ReadLine();
+
+            Console.WriteLine("ingresa numero2: ");
+            n2 = Console.ReadLine();
+
+            //conversion de variables a tipo int
+            int numero1 = int.Parse(n1);
+            int numero2 = int.Parse(n2);
+
+            //operaciones
+            int suma = numero1 + numero2;
+            int multi = numero1 * numero2;
+            int resta = numero1 - numero2;
+            int division = numero1 / numero2;
+            int residuo = numero1 % numero2;
+
+            //mostrar en pantalla
+            //Console.WriteLine("HOLA");
+            Console.WriteLine("LA SUMA ES: {0}", suma);
+            Console.WriteLine("LA RESTA ES: {0} ", resta);
+            Console.WriteLine("LA DIVISON ES: {0} ", division);
+            Console.WriteLine("LA MULTIPLICACION ES: {0} ", multi);
+            Console.WriteLine("EL RESIDUO ES: {0} ", residuo);
+
+            Console.ReadKey();
+
+        }
+    }
+}
