af40bdd64c0056fd49a02d2e7b27e6e4aaea16a0
Luis Fernando Gomez Saldaña <fernando1894@hotmail.com>
2021-03-23 16:02:24 -0700

    se anadio el proyecta de comida para gatos

---
+++ Practica_2/TiendaParaGatos/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace TiendaParaGatos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double latas, total = 0;
+            String line;
+            
+            Console.WriteLine("¡Bienvenido a la tienda de comida para gatos!");
+
+            Console.WriteLine("Ingresa el numero de latas que desea comprar: ");
+            line = Console.ReadLine();
+            latas = int.Parse(line);
+
+            if(latas < 50)
+            {
+                total = latas * 2;
+            }
+            else if(latas >= 50 && latas < 100)
+            {
+                total = latas * 1.75;
+            }
+            else if(latas >= 100)
+            {
+                total = latas * 1.5;
+            }
+
+            Console.WriteLine("Cantidad: " + latas + " Costo total: $" + total + " MXN");
+        }
+    }
+}
